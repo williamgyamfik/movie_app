360b8e569de8ce88b97f40bae25a1191758b6253
williamgyamfik <chester3gh@gmail.com>
2023-02-01 03:13:57 +0200

    redesigned form component

--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,8 @@
 import { useState, useRef, useContext } from "react";
 import AuthContext from "../store/AuthContext";
 import { useRouter } from "next/router";
-// import { useNavigate } from "react-router-dom";
+
+import movieBackDrop from "./movieBackDrop";
 
 const Form = () => {
   const router = useRouter();
@@ -76,84 +77,85 @@ const Form = () => {
   };
 
   return (
-    <div className="text-light container h-100 pt-5">
-      <div
-        className="row h-100 justify-content-center align-items-center "
-        style={{ paddingTop: "9rem" }}
+    <div className="text-light container h-100 ">
+      <form
+        className=" pt-3 pb-5  bg-dark col-md-12 col-sm-12 col-12 col-lg-12"
+        onSubmit={submitHandler}
       >
-        <form className=" pt-3 pb-5 w-25 bg-dark " onSubmit={submitHandler}>
-          <h2 className="text-center">{signIn ? "Sign in" : "Sign up"}</h2>
-          <div className="row mb-3 justify-content-center">
-            <div className="col-sm-10 pb-5 pt-5">
-              <input
-                type="email"
-                className="form-control form-control-lg bg-dark text-light"
-                id="inputEmail3"
-                placeholder="Email"
-                ref={emailInputRef}
-              />
-            </div>
+        <h1
+          className="text-center fw-bold "
+          style={{ fontSize: "3rem", color: "red" }}
+        >
+          Welcome to WatchFlix, {signIn ? "Sign in" : "Sign up"}
+        </h1>
+        <div className="row  justify-content-center pt-5">
+          <div className="col-sm-10 col-md-6 col-lg-6 col-10 pt-3">
+            <input
+              type="email"
+              className="form-control form-control-lg bg-dark text-light"
+              id="inputEmail3"
+              placeholder="Email"
+              ref={emailInputRef}
+            />
           </div>
-          <div className="row mb-3 justify-content-center">
-            <div className="col-sm-10">
-              <input
-                type="password"
-                className="form-control form-control-lg bg-dark text-light"
-                id="inputPassword3"
-                placeholder="Password"
-                ref={passwordInputRef}
-              />
-            </div>
+        </div>
+        <div className="row  justify-content-center">
+          <div className="pt-3 col-sm-10 col-md-6 col-lg-6 col-10">
+            <input
+              type="password"
+              className="form-control form-control-lg bg-dark text-light"
+              id="inputPassword3"
+              placeholder="Password"
+              ref={passwordInputRef}
+            />
           </div>
-          <div className="text-center pb-3 pt-5">
-            {!loading && (
-              <button
-                type="submit"
-                className="btn col-sm-10 "
-                style={{ color: "white", backgroundColor: "red" }}
-              >
-                {signIn ? "sign in" : "sign up"}
-              </button>
-            )}
-            {loading && (
-              <p className="text-light text-center">Sending request.....</p>
-            )}
+        </div>
+        <div className="text-center pb-3 pt-5">
+          {!loading && (
+            <button
+              type="submit"
+              className="btn col-10 col-sm-10 col-md-6 col-lg-6 "
+              style={{ color: "white", backgroundColor: "red" }}
+            >
+              {signIn ? "sign in" : "sign up"}
+            </button>
+          )}
+          {loading && (
+            <p className="text-light text-center">Sending request.....</p>
+          )}
+        </div>
+
+        <div className="form-check d-flex justify-content-evenly  fs-6">
+          <div className="d-flex justify-content-center">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault1"
+            />
+            <label className="form-check-label" htmlFor="flexRadioDefault1">
+              Remember me
+            </label>
+          </div>
+          <p>Need Help?</p>
+        </div>
+        <div className="fs-6 text-center">
+          <div className="d-flex justify-content-center align-items-center pt-3">
+            {/* <p>New to Watchflix?</p> */}
+            <button
+              className="btn btn-success col-10 col-sm-10 col-md-6 col-lg-6"
+              onClick={signInSwitchHandler}
+            >
+              {signIn ? "Sign up" : "Sign in"}
+            </button>
           </div>
 
-          <div className="form-check d-flex justify-content-between fs-6s">
-            <div className="d-flex justify-content-endss">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                id="flexRadioDefault1"
-              />
-              <label className="form-check-label" htmlFor="flexRadioDefault1">
-                Remember me
-              </label>
-            </div>
-            <p>Need Help?</p>
-          </div>
-          <div className="fs-6">
-            <div>
-              <p>
-                New to Watchflix?{" "}
-                <button
-                  className="btn btn-success"
-                  onClick={signInSwitchHandler}
-                >
-                  {signIn ? "Sign up" : "Sign in"}
-                </button>
-              </p>
-            </div>
-
-            <p>
-              This page is protected by Google reCAPTCHA to ensure you're not a
-              bot.
-            </p>
-          </div>
-        </form>
-      </div>
+          <p className="pt-3">
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot.
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
